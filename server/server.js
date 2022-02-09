591b7588f1358d36d7261f8eaf9e86eb69ef13e3
Leandre Chretiennot <leandre.chretiennot@epitech.eu>
2022-02-09 10:50:06 +0100

    [UPDATE] separate client and server

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 const path = require('path');
 app.use(cors());
+const pathToClient = __dirname + '/../client'
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+   res.sendFile(path.resolve(pathToClient, 'public', 'index.html'));
 });
 app.listen(port, () => {
    console.log(`Server is up at port ${port}`);
