ec8587904cf902962b63d183c7a4ae84fdcf2636
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-02-16 11:07:09 +0100

    [MODIFY] new router

--- client/src/components/Home/Home.svelte
+++ client/src/components/Home/Home.svelte
@@ -1,21 +1,21 @@
 <script>
-    import { onMount } from "svelte";
-    let test;
+  import { onMount } from "svelte";
+  let test;
 
-    onMount(async () => {
-        const res = await fetch(
-            "https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-cibul"
-        );
-        const response = await res.json();
-        console.log(response);
-        test = response;
-    });
+  onMount(async () => {
+    const res = await fetch(
+      "https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-cibul"
+    );
+    const response = await res.json();
+    console.log(response);
+    test = response;
+  });
 </script>
 
 {#if test !== undefined}
-    <div>
-        {#each test.records as record}
-            <p>{record.fields.title}</p>
-        {/each}
-    </div>
+  <div>
+    {#each test.records as record}
+      <p>{record.fields.title}</p>
+    {/each}
+  </div>
 {/if}
