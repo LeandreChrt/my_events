02d0faee74b46a0c5a2bcdd498d21d41760a2ff1
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-02-17 01:05:05 +0100

    [ADD] component for menu

---
+++ client/src/components/Menu.svelte
@@ -0,0 +1,28 @@
+<script></script>
+
+<div class="sidebar">
+  <div class="content">
+    <h3>Filtres</h3>
+    <select>
+      <option value="Catégories" default />
+      <option value="test1">Test1</option>
+      <option value="test2">Test2</option>
+      <option value="test3">Test3</option>
+    </select>
+    <input type="text" placeholder="Lieu" />
+  </div>
+</div>
+
+<style>
+  .sidebar {
+    background-color: green;
+    border: solid 1px black;
+    width: 30%;
+    height: 90%;
+    margin: 1em 0px;
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
