4df5f441f93d7214b43ce7968b15e1e1f6fe80dc
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-02-17 01:06:33 +0100

    [RENAME] home => callApi

---
+++ client/src/components/callApi.svelte
@@ -0,0 +1,20 @@
+<script>
+  const fetchAPI = (async () => {
+    const response = await fetch(
+      "https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-cibul"
+    );
+    return await response.json();
+  })();
+</script>
+
+{#await fetchAPI}
+  <p>...waiting</p>
+{:then data}
+  {console.log(data)}
+  {#each data.records as value}
+    <p>{value.datasetid}</p>
+  {/each}
+  <p>Bonjour</p>
+{:catch error}
+  <p>An error occurred!</p>
+{/await}
