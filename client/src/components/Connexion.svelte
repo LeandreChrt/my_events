082e8eb4ddf2364f2b9c8055f90a8dd8002aceea
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-02-17 01:02:59 +0100

    [MODIFY] add css rules + comment EventDispatcher

--- client/src/components/Connexion.svelte
+++ client/src/components/Connexion.svelte
@@ -1,20 +1,32 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-  const dispatch = createEventDispatcher();
+  //   import { createEventDispatcher } from "svelte";
+  import AuthModal from "./AuthModal.svelte";
+  //   const dispatch = createEventDispatcher();
+
+  let modal;
 </script>
 
 <div>
-  <button on:click={() => dispatch("auth")} />
+  <button
+    on:click={() => {
+      modal.show();
+    }}
+  />
 </div>
 
+<AuthModal bind:this={modal}>
+  <h1>coucou</h1>
+</AuthModal>
+
 <style>
-  div {
-    border: solid 1px black;
-  }
   button {
     background-color: black;
     border: none;
     height: 100%;
     width: 50px;
+  }
+
+  button:hover {
+    border-radius: 30%;
   }
 </style>
