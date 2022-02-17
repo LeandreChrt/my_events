28ebc33e7918145f264d7db2eef8473be2c503d0
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-02-17 01:54:34 +0100

    [MODIFY] add 404 error route

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,9 +2,11 @@
   import Router from "svelte-spa-router";
   import Header from "./components/Header.svelte";
   import HomeBody from "./components/HomeBody.svelte";
+  import LostPage from "./components/404.svelte";
 
   let routes = {
     "/": HomeBody,
+    "*": LostPage,
   };
 </script>
 
