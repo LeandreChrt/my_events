48651a9398615fc6331e3342098ce26b8059f050
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-02-17 01:17:53 +0100

    [MODIFY] uncomment router + modify structure

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,17 +1,12 @@
 <script>
-  // import Router from "svelte-spa-router";
+  import Router from "svelte-spa-router";
   import Header from "./components/Header.svelte";
-  import Menu from "./components/Menu.svelte";
-  import Home from "./components/Home.svelte";
+  import HomeBody from "./components/HomeBody.svelte";
 
-  // let routes = {
-  //   "/": Home,
-  // };
+  let routes = {
+    "/": HomeBody,
+  };
 </script>
 
 <Header />
-<Menu />
-
-<main>
-  <!-- <Router {routes} /> -->
-</main>
+<Router {routes} />
