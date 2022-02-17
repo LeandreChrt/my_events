4b3867f277df509b2307a578514b3599022a6bda
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-02-17 01:55:05 +0100

    [MODIFY] prepare display of event + add style

--- client/src/components/HomeBody.svelte
+++ client/src/components/HomeBody.svelte
@@ -2,12 +2,22 @@
   import Menu from "./Menu.svelte";
 </script>
 
-<Menu />
-
-<main />
+<main>
+  <Menu />
+  <div class="events">
+    <h2>Events à venir</h2>
+  </div>
+</main>
 
 <style>
   main {
+    display: flex;
+    width: 100%;
+    height: 100%;
     background: blue;
+    color: white;
+  }
+  .events {
+    padding: 1em;
   }
 </style>
