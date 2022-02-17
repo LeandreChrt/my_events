fdfc88cc7d5605d0f2dd2c5f1945d55f5e23baf1
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-02-17 01:00:52 +0100

    [MODIFY] comment router for now + Add header and menu components

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,19 +1,17 @@
 <script>
-  import Router from "svelte-spa-router";
+  // import Router from "svelte-spa-router";
   import Header from "./components/Header.svelte";
+  import Menu from "./components/Menu.svelte";
   import Home from "./components/Home.svelte";
 
-  let routes = {
-    "/": Home,
-  };
+  // let routes = {
+  //   "/": Home,
+  // };
 </script>
 
 <Header />
-
-<nav>
-  <a href="#/">Home</a>
-</nav>
+<Menu />
 
 <main>
-  <Router {routes} />
+  <!-- <Router {routes} /> -->
 </main>
