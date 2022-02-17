35b7c62c9946fbfedc49d1079e2b0be5f1204a8a
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-02-17 02:26:42 +0100

    [MODIFY] create event block + styling

--- client/src/components/callApi.svelte
+++ client/src/components/callApi.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { loop_guard } from "svelte/internal";
+
   const fetchAPI = (async () => {
     const response = await fetch(
       "https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-cibul"
@@ -12,9 +14,27 @@
 {:then data}
   {console.log(data)}
   {#each data.records as value}
-    <p>{value.datasetid}</p>
+    <div class="event">
+      <h3>{value.fields.title}</h3>
+      <p>{value.fields.description}</p>
+      <p>du {value.fields.date_start} jusqu'au {value.fields.date_end}</p>
+      <button class="goTo">En savoir +</button>
+    </div>
   {/each}
-  <p>Bonjour</p>
 {:catch error}
   <p>An error occurred!</p>
 {/await}
+
+<style>
+  .event {
+    position: relative;
+    margin: 1px;
+    background-color: brown;
+  }
+
+  .goTo {
+    position: absolute;
+    right: 5%;
+    bottom: -20%;
+  }
+</style>
